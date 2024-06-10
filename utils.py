703b373c133869b0de562b8ec45195eef338f89d
leonhard.stengel@outlook.de <leonhard.stengel@outlook.de>
2024-06-10 14:42:56 +0200

    feat: tweet preprocessor

--- utils.py
+++ utils.py
@@ -2,40 +2,60 @@ import re
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+from nltk.tokenize import TweetTokenizer
+import string
 
-def window_making(tweets):
-    pass
+class TweetPreprocessor:
 
-def remove_noise(tweet):
+    def process_tweets(self, tweets):
+        normalized_tweets = []
+        for tweet in tweets:
+            tweet_tokens = self.normalize_tweet(tweet)
+            if len(tweet_tokens) > 0:
+                normalized_tweets.append(tweet_tokens)
+        print(normalized_tweets)
+        return normalized_tweets
+        #return window_making(normalized_tweets)
 
-    #make tweet lower case
-    lower = tweet.lower()
+    def window_making(self, tweets):
+        pass
 
-    # tokenize
-    word_tokens = word_tokenize(lower)
-    
-    #remove tweets with less than 3 words
-    if len(word_tokens) < 3:
-        return ''
+    def normalize_tweet(self, tweet):
 
-    #remove stop words
-    nltk.download('stopwords')
-    stop_words = set(stopwords.words('english'))
-    word_tokens = [w for w in word_tokens if not w in stop_words]
+        tokenizer = TweetTokenizer()
+        #make tweet lower case
+        lower = tweet.lower()
 
-    #remove punctuation
-    pass
+        # tokenize
+        word_tokens = tokenizer.tokenize(lower)
 
-    #remove web-lins
-    pass
+        #remove tweets with less than 3 words
+        if len(word_tokens) < 3:
+            return []
 
-    #remove non-ASCII characters
-    pass
+        #remove stop words
+        #nltk.download('stopwords')
+        stop_words = set(stopwords.words('english'))
+        word_tokens = [w for w in word_tokens if not w in stop_words]
 
-    #remove retweets
-    pass
+        #remove web-lins
+        word_tokens = [w for w in word_tokens if not w.startswith('http') and not w.startswith('www')]
 
-    return ' '.join(word_tokens)
+        #remove punctuation
+        word_tokens = [''.join(c for c in w if c not in string.punctuation or c == '#') for w in word_tokens]
 
+        #remove non-ASCII characters
+        word_tokens = [w.encode('ascii', 'ignore').decode() for w in word_tokens]
 
+        #remove retweet
+        if False:
+            return []
 
+        #remove empty tokens
+        word_tokens = [w for w in word_tokens if w]
+
+        return word_tokens
+
+processor = TweetPreprocessor()
+
+processor.process_tweets(["This is https://www.google.de a Test #Tweet 🤨 Huhu and it is a Test, juju.","Yes it Test and Test, test."])
