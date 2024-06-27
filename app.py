2d71e43c2a3bd426be0644d58e970c0e83369656
yousseflotfy88 <youssef_ayman2011@hotmail.com>
2024-06-27 19:39:24 +0200

    structural relationship done

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from utils.Dataloader import Dataloader
 from utils.TweetPreprocessor import TweetPreprocessor
 from graph.ContextualKnowledge import ContextualEmbeddings
+from graph.structural_relation import StructuralRelation
 
 import numpy as np
 
@@ -11,7 +12,7 @@ dataloader = Dataloader()
 preprocessor = TweetPreprocessor()
 
 contextual = ContextualEmbeddings()
-
+structural = StructuralRelation()
 #@app.route("/events")
 #def fetch_events():
 
@@ -21,9 +22,9 @@ windows = dataloader.window_making(60, data)
 for name, window in windows:
     tweets = window.to_dict('records')
     tweets = preprocessor.process_tweets(tweets)
-    
+    # print(tweets[0])
     contextual_similarity = contextual.get_similarity_matrix(tweets)
-    structural_similarity = contextual_similarity #TODO: structual similarity
+    structural_similarity =  structural.get_struct_relation_matrix(tweets)#TODO: structual similarity
 
     graph_matrix = 0.5 * (contextual_similarity + structural_similarity)
     # Get highest scores
